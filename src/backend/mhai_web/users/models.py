8ff7961db2cdba7a3d230d8cabe0504f98b63f91
Sandro Loch <es.loch@gmail.com>
2024-10-25 05:05:51 -0300

    fix(settings) Fix databases in base settings

--- src/backend/mhai_web/users/models.py
+++ src/backend/mhai_web/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField, EmailField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from simple_history.models import HistoricalRecords  # Import for tracking history
+from simple_history.models import HistoricalRecords
 
 from .managers import UserManager
 
@@ -26,7 +26,6 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    # Add HistoricalRecords to track changes to this model
     history = HistoricalRecords()
 
     objects: ClassVar[UserManager] = UserManager()
