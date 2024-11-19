1af787b1d10a0f0c77b6401cdb97dd7895696f00
Sandro Loch <es.loch@gmail.com>
2024-11-19 11:14:54 -0300

    feat(settings): prevent import errors for historical user modules in base settings

--- src/backend/config/settings/base.py
+++ src/backend/config/settings/base.py
@@ -19,6 +19,10 @@ if READ_DOT_ENV_FILE:
 # -----------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool("DJANGO_DEBUG", False)
+
+# Local dev settings
+SHELL_PLUS_DONT_LOAD = ["*"]
+
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
